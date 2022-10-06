e9918356090ba85d9df574abcb122ec1c2e8b15d
HyEISN <hyeisn@hotmail.com>
2022-10-06 18:04:14 +0800

    update

--- Esp32/S3/Train/blink/blink.ino
+++ Esp32/S3/Train/blink/blink.ino
@@ -27,6 +27,11 @@ void setLEDColor(uint8_t pin, int red, int green, int blue)
     neopixelWrite(pin, colorCast(red), colorCast(green), colorCast(blue));
 }
 
+double setColorCond(int color, double val) 
+{
+    return color < 0 ? abs(val) : -abs(val);
+}
+
 void setup()
 {
     // No need to initialize the RGB LED
@@ -47,6 +52,20 @@ void loop()
 
         if ((i + 1) > arrSize - 1) 
         {
+            const int *nextColor = COLOR_VECTOR[i + 1];
+            double swapColor[] = {nextColor[0] - color[0], nextColor[1] - color[1], nextColor[2] - color[2]};
+            int sColorR = abs(swapColor[0]);
+            int sColorG = abs(swapColor[1]);
+            int sColorB = abs(swapColor[2]);
+            // int mColorR = sColorR / 1000;
+            // int mColorG = sColorG / 1000;
+            // int mColorB = sColorB / 1000;
+
+            for (size_t i = 0; i < 1000 / CHANGE_RATE; i++)
+            {
+                setLEDColor(RGB_BUILTIN, colorCast(sColorR), colorCast(sColorG), colorCast(sColorB));
+                delay(CHANGE_RATE);
+            }
             
         }
 
